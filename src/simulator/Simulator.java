7be96eca6fe3bb7dc0824e185f1219c6528cc3fb
TheGodOfAwesome <muvezwakuzivakwashe@yahoo.com>
2018-06-25 12:09:44 +0200

    changing project root on local

---
+++ src/simulator/Simulator.java
@@ -0,0 +1,13 @@
+package simulator;
+
+public class Simulator {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hi there!");
+        if (args.length > 0)
+            System.out.println(args[0]);
+        return;
+    }
+
+}
